1b749457f8aa14eaa5d456822817b0bd52a0923c
m0ntbl4ck <mont.black.123@gmail.com>
2023-09-07 16:07:26 -0500

    ADD GITIGNORE

--- src/soccer/play/League.java
+++ src/soccer/play/League.java
@@ -1,4 +1,7 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package soccer.play;
 
 /**
@@ -6,5 +9,12 @@ package soccer.play;
  * @author mont_
  */
 public class League {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
